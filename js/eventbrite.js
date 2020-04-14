f74555fd7b74f50f735afdc8d431775f819df1b4
Catalina Carreto <isccatalinacarreto27@gmail.com>
2020-04-14 13:48:57 -0500

    feat: Crear la aplicacion y clases

---
+++ js/eventbrite.js
@@ -0,0 +1,12 @@
+class EventBrite{
+    constructor(){
+        this.token_auth = '3UYJ7LC6A5ZA3CVWDYRM';
+        this.ordenar = 'date';
+    }
+
+    //Obtiene las categorias en init()
+    async obtenerCategorias(){
+        //Consultar las categorias a la REST API de event brite
+        
+    }
+}
---
+++ js/interfaz.js
@@ -0,0 +1,16 @@
+class Interfaz{
+    constructor(){
+        //Inicializa la App al instanciar
+        this.init();
+        //leer el resultado
+        this.listado = document.getElementById('resultado-eventos');
+    }
+    //Metodo para cuando inicialice la App
+    init(){
+
+    }
+    //Imprimir categorias
+    imprimirCategorias(){
+        const listaCategorias = eventbrite.obtenerCategorias
+    }
+}
